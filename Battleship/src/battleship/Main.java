c98953159cb96f516c3eb9e4ed383903d0779aae
Weiner <Weiner@alon.mynet>
2018-08-14 17:08:22 +0300

    Creation of Battleship class: constructor(rows, columns) Print method

--- Battleship/src/battleship/Main.java
+++ Battleship/src/battleship/Main.java
@@ -1,10 +1,31 @@
 package battleship;
 
-public class Main {
+import java.util.Scanner;
+
+public  class Main {
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int rows;
+		int columns;
+		
+		
+		//Scanner implementation
+		Scanner input = new Scanner(System.in);
+		 
+		System.out.println("Enter number of rows");
+		rows = input.nextInt();
+		
+		System.out.println("Enter number of columns");
+		columns = input.nextInt();
+		
+		//Creates a rows x columns size game.
+		Battleship game = new Battleship(rows,columns);
+		
+		game.print();
+		
+		
 	}
 
 }
